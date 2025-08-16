0f0870e5107fc419b79159215c6f829e36aa6176
Sudhanva <147122943+Sudhanva420@users.noreply.github.com>
2025-08-16 11:55:44 +0530

    Update cluster3_subscriber2.py

--- FINAL/cluster3_subscriber2.py
+++ FINAL/cluster3_subscriber2.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 
 def subscriber():
     # Specific endpoint for this subscriber
-    websocket_url = 'ws://localhost:5004/sub8'
+    websocket_url = 'ws://localhost:5007'
     
     consumer = KafkaConsumer(
         'cluster3-topic-subscribers',
